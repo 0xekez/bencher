fa76d478bd4f2f4503f143e895ca2cd69668cb7a
Zeke Medley <zekemedley@gmail.com>
2020-11-22 18:42:54 -0800

    set up requests via public and private ip addresses

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use bencher::{self, config};
 
 fn main() {
+    // Serves file at http://localhost:8000/10mb.pdf. Note that this
+    // will show some error output. This isn't really an issue. See:
+    // https://github.com/SergioBenitez/Rocket/issues/286
     let handle = bencher::server::start_on_new_thread();
     let config = config::get_config();
-    bencher::requester::do_file_requests(&config);
-    handle.join();
+
+    // We unwrap these as an error here is presumed to be fatal.
+    bencher::requester::do_file_requests(&config).unwrap();
+    handle.join().unwrap();
 }
--- src/requester.rs
+++ src/requester.rs
@@ -1,11 +1,23 @@
 use crate::config;
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::Write;
 use std::thread;
-use std::time::{Duration, Instant};
 
-fn send_file_request(config: &config::Config) -> Result<(), String> {
-    let url = format!("http://{}/{}", config.peer_ip, config.peer_file);
+/// Essentially a wrapper for opening a file that creates it if it
+/// doesn't exist and appends to it if it does.
+fn open_logfile(name: &String) -> Result<File, String> {
+    OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(name.clone())
+        .map_err(|e| e.to_string())
+}
+
+/// Sends a request to the specified ip for a specified file. Returns
+/// Ok if the request succedes and an error with information about the
+/// failure code otherwise.
+fn send_file_request(peer_ip: &String, peer_file: &String) -> Result<(), String> {
+    let url = format!("http://{}/{}", peer_ip, peer_file);
     let resp = reqwest::blocking::get(&url).map_err(|e| e.to_string())?;
     if resp.status().is_success() {
         Ok(())
@@ -14,26 +26,51 @@ fn send_file_request(config: &config::Config) -> Result<(), String> {
     }
 }
 
-pub fn do_file_requests(config: &config::Config) -> Result<(), String> {
-    let mut logfile = OpenOptions::new()
-        .append(true)
-        .open(config.request_log_file.clone())
-        .map_err(|e| e.to_string())?;
-    let mut errfile = OpenOptions::new()
-        .append(true)
-        .open(config.request_error_file.clone())
-        .map_err(|e| e.to_string())?;
-    loop {
-        let start = Instant::now();
-        match send_file_request(config) {
-            Ok(_) => {
-                let duration = start.elapsed();
-                writeln!(logfile, "{:?}", duration).map_err(|e| e.to_string())?;
-            }
-            Err(what) => {
-                writeln!(errfile, "{}", what).map_err(|e| e.to_string())?;
-            }
+/// Sends a file request and logs information about it to the
+/// logfiles. Information about each request is written to the log
+/// file in the form <request start time>, <request elapsed time>.
+fn do_file_request(
+    peer_ip: &String,
+    peer_file: &String,
+    logfile: &mut File,
+    errfile: &mut File,
+) -> Result<(), String> {
+    let start = std::time::Instant::now();
+    match send_file_request(peer_ip, peer_file) {
+        Ok(_) => {
+            let duration = start.elapsed();
+            writeln!(logfile, "{:?}, {:?}", start, duration).map_err(|e| e.to_string())
         }
-        thread::sleep(Duration::from_secs(5));
+        Err(what) => writeln!(errfile, "{}", what).map_err(|e| e.to_string()),
+    }
+}
+
+/// Sends file requests to the peer ip addresses at a fixed
+/// interval. This should never return unless an error occured.
+pub fn do_file_requests(config: &config::Config) -> Result<(), String> {
+    let mut pub_logfile = open_logfile(&config.pub_request_log_file)?;
+    let mut pub_errfile = open_logfile(&config.pub_request_err_file)?;
+
+    let mut priv_logfile = open_logfile(&config.priv_request_log_file)?;
+    let mut priv_errfile = open_logfile(&config.priv_request_err_file)?;
+
+    loop {
+        // Make a file request via the public ip address.
+        do_file_request(
+            &config.peer_public,
+            &config.peer_file,
+            &mut pub_logfile,
+            &mut pub_errfile,
+        )?;
+
+        // Make a file request via the private ip address.
+        do_file_request(
+            &config.peer_private,
+            &config.peer_file,
+            &mut priv_logfile,
+            &mut priv_errfile,
+        )?;
+
+        thread::sleep(std::time::Duration::from_secs(10));
     }
 }
