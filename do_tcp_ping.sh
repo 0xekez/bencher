49f77f436301f5bf98775441c7ca579c1312f5fb
Zeke Medley <zekemedley@gmail.com>
2021-03-14 22:07:32 -0700

    clarifying comment

--- do_tcp_ping.sh
+++ do_tcp_ping.sh
@@ -1,1 +1,16 @@
-nmap -sS -Pn -n -p$2 $1 | rg latency | rg "[[:digit:]]+\\.[[:digit:]]+" --only-matching
+# Getting these commands to pipe together properly in Rust is a pain
+# and I am lazy / don't have the patience to fight it so we just
+# execute this shell script instead.
+
+# Takes an ip as argument 1 and a port as argument 2 and runs nmap to
+# calculate the latency over the tcp connection. Netmap calculates
+# latency by sending a SYN packet as if it was initiating a tcp
+# connection and timing how long the corresponding SYN/ACK packet
+# takes to arrive.
+#
+# I've done some adhoc testing of this and the results seem to very
+# closely match that of regular ping execution.
+
+nmap -sS -Pn -n -p$2 $1 | \
+    rg latency | \
+    rg "[[:digit:]]+\\.[[:digit:]]+" --only-matching
