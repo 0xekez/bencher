78df85da488858005a32841a6dac0f5dfafabc07
Billy <billyz@berkeley.edu>
2021-11-29 23:06:33 -0800

    to vm

--- miniboxes/benching/start_cpu_and_signal.sh
+++ miniboxes/benching/start_cpu_and_signal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mpstat 1 >> $1 &
-ssh 192.168.200.172 "qperf -t 60 192.168.200.171 tcp_bw" >> $2
-ssh 192.168.200.172 "qperf -t 60 192.168.200.171 tcp_lat" >> $3
+ssh minibox@192.168.200.172 "qperf -t 60 192.168.200.171 tcp_bw" >> $2
+ssh minibox@192.168.200.172 "qperf -t 60 192.168.200.171 tcp_lat" >> $3
 pkill mpstat
